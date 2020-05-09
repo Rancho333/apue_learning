5f9d9858d667cc810f6f8ac0bffee9104d37c994
Rancho333 <zhwwan@gmail.com>
2020-05-09 11:48:44 +0800

    First commit for apue

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # apue_learning
+
+This is the project for learning apue!
---
+++ chapter1/getpid.c
@@ -0,0 +1,7 @@
+#include "../apue.h"
+
+int main(void)
+{
+    printf("This program pid is:%ld\n", (long)getpid());
+    return 0;    
+}
